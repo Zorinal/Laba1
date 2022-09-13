a0f9a70b65a7beb0bc07c2e28abe3a08fa48db36
DmitriyChepasov <95770579+Zorinal@users.noreply.github.com>
2022-09-14 00:46:44 +0300

    21a
    asd

--- Лаба1.c
+++ Лаба1.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
 int index_first_zero(int arr[]) {
-  int c = 0;
+ fprintf(stderr,"text1"); 
+int c = 0;
   for (int i = 0; i < 100; i++) {
     if (arr[i] == 0) {
-      c = i;
+ 
+     c = i;
       break;
     }
+fprintf(stderr,"text2"); 
+
   }
   return c;
 }
 int index_last_zero(int arr[]) {
-  int c = 0;
+fprintf(stderr,"text3");
+ int c = 0;
   for (int i = 99; i >= 0; i--) {
     if (arr[i] == 0) {
       c = i;
       break;
     }
-  }
+  }fprintf(stderr,"text4");
+
   return c;
 }
 int sum_between(int arr[]) {
+fprintf(stderr,"text5");
   int sum = 0;
   int checkfirst = 0;
   int checklast = 0;
@@ -28,41 +35,43 @@ int sum_between(int arr[]) {
      { checkfirst = i;
      break;
       }
-  }
+  }fprintf(stderr,"text6");
+
   for (int i = 99; i >= 0; i--) {
     if (arr[i] == 0)
      { checklast = i;
      break; }
-  }
+  }fprintf(stderr,"text7");
   for (int i = checkfirst; i <= checklast; i++) {
     sum += arr[i];
-  }
+  }fprintf(stderr,"text8");
   return sum;
 }
  
 int sum_before_and_after(int arr[]) {
-  int sum = 0;
+ fprintf(stderr,"text9"); 
+ int sum = 0;
   int sum1 = 0;
   for (int i = 0; i < 100; i++) {
     if (arr[i] != 0)
       sum += arr[i];
     else break;
-  }
-  for (int i = 99; i >= 0; i--) {
+  }printf("text10");
+  for (int i = 99; i >= 0; i--) {fprintf(stderr,"text11");
     if (arr[i] != 0)
       sum += arr[i];
     else break;
-  }
+  } fprintf(stderr,"text12");
   return sum;
 }
 int main() {
   int n;
   scanf("%d", n);
   int arr[100];
-  char line = ' ';
+  char line = ' ';fprintf(stderr,"text13");
   for (int i = 0; line != "\n"; i++) {
     scanf("%d %c", & arr[i], & line);
-  }
+  }fprintf(stderr,"text14");
    switch(n){
       case 0:
           printf("%d\n ", index_first_zero(arr)); 
@@ -82,5 +91,6 @@ int main() {
     printf("%s\n", "Данные некорректны");
 		break;
 }
+fprintf(stderr,"text15");
 return 0;
 }
